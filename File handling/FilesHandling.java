aedffd3d1d209c7b8dc8e6c78a6d49b2fd344cd7
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2025-09-05 14:57:04 +0530

    .

---
+++ File handling/FilesHandling.java
@@ -0,0 +1,21 @@
+/*
+ * file handling -- for storing data permanently in  file
+ * we read data from file and write into the file 
+ *  stream-- flow of data
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+
+public class FilesHandling {
+    
+}
---
+++ Threading/PredefinedMethod.java
@@ -0,0 +1,22 @@
+
+/* isAlive()// to check is live or not 
+ * .sleep(milisecod)// wait for some time
+ * .join() //current thread wait until first working thread end
+ * .interrupt() //use to block thread(sleep(), wait(), join()) then throw expection
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+
+public class PredefinedMethod {
+    
+}
