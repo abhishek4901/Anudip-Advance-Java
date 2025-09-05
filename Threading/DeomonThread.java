6bd500b0dacffd7bef40cd54c8af0cd66f53f712
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2025-09-05 14:39:13 +0530

    .

--- Threading/DeomonThread.java
+++ Threading/DeomonThread.java
@@ -4,7 +4,48 @@
  * we can make user thread -- setDemon(true)
  */
 
-public class DeomonssThread {
-    
+public class DeomonThread {
+
+    // Thread A
+    static class A extends Thread {
+        public void run() {
+            System.out.println("Thread ID: " + Thread.currentThread().getId());
+            System.out.println("Thread State: " + Thread.currentThread().getState());
+            System.out.println("Thread Name: " + Thread.currentThread().getName());
+            System.out.println("Thread Priority: " + Thread.currentThread().getPriority());
+
+            for (int i = 5; i <= 50; i += 5) {
+                System.out.println("A: " + i);
+            }
+        }
+    }
+
+    // Thread B (Daemon thread)
+    static class B extends Thread {
+        public void run() {
+            System.out.println("Thread ID: " + Thread.currentThread().getId());
+            System.out.println("Thread State: " + Thread.currentThread().getState());
+            System.out.println("Thread Name: " + Thread.currentThread().getName());
+            System.out.println("Thread Priority: " + Thread.currentThread().getPriority());
+            System.out.println("Is Daemon: " + Thread.currentThread().isDaemon());
+
+            for (int i = 7; i <= 70; i += 7) {
+                System.out.println("B: " + i);
+            }
+        }
+    }
+
+    // Main method
+    public static void main(String[] args) {
+        A a1 = new A();
+        B b1 = new B();
+
+        a1.setName("My Thread-1");
+        a1.setPriority(Thread.MAX_PRIORITY); // Highest priority
+
+        b1.setDaemon(true); // Mark B as Daemon thread
+
+        a1.start();
+        b1.start();
+    }
 }
-  
